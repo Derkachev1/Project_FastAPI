b51eb4ac4d3e723824d2bc82aae7638d8e03dfef
Derkachev1 <derkachev.04@mail.ru>
2024-12-23 20:24:07 +0300

    FastAPI router and settings

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Union
+
+from app.config import settings
+
+from app.__init__ import router as api_router
 
 
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix,
+)
 
 
 @app.get("/test")
@@ -13,3 +20,6 @@ def test():
 @app.get("/")
 def home():
     return "Hello World"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=settings.run.host, port=settings.run.port, reload=True)
